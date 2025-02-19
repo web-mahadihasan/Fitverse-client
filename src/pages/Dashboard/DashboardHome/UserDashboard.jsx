5394d5dd3c9659845c58a40b444064860eb3789e
web-mahadihasan <mehedihasanmilu7@gmail.com>
2025-02-19 19:24:23 +0600

    Update user chart by price

--- src/pages/Dashboard/DashboardHome/UserDashboard.jsx
+++ src/pages/Dashboard/DashboardHome/UserDashboard.jsx
@@ -2,11 +2,12 @@ import { UserIcon, UsersIcon, ClipboardIcon, FlameIcon as FireIcon } from "lucid
 import InfoCard from "../../../components/common/InfoCard";
 import useAxiosSecured from "../../../hooks/useAxiosSecured";
 import { useQuery } from "@tanstack/react-query";
-import ClassComparisonChart from "./ClassComparisonChart";
-import useAdmin from "../../../hooks/useAdmin";
 import AnimatedLoader from "../../Loading/Loading";
 import { FaMoneyBillTransfer } from "react-icons/fa6";
 import useAuth from "../../../hooks/useAuth";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, ResponsiveContainer,  Tooltip, Legend, } from 'recharts';
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
 
 const UserDashboard = () => {
     const axiosSecured = useAxiosSecured()
@@ -23,6 +24,64 @@ const UserDashboard = () => {
     });
    
     const {userPayment, bookedTrainer} = userDashboardData || {}
+    // console.log(userPayment)
+    const data = [
+        {
+          name: 'Page A',
+          uv: 4000,
+          pv: 2400,
+          amt: 2400,
+        },
+        {
+          name: 'Page B',
+          uv: 3000,
+          pv: 1398,
+          amt: 2210,
+        },
+        {
+          name: 'Page C',
+          uv: 2000,
+          pv: 9800,
+          amt: 2290,
+        },
+        {
+          name: 'Page D',
+          uv: 2780,
+          pv: 3908,
+          amt: 2000,
+        },
+        {
+          name: 'Page E',
+          uv: 1890,
+          pv: 4800,
+          amt: 2181,
+        },
+        {
+          name: 'Page F',
+          uv: 2390,
+          pv: 3800,
+          amt: 2500,
+        },
+        {
+          name: 'Page G',
+          uv: 3490,
+          pv: 4300,
+          amt: 2100,
+        },
+      ];
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+        ${x + width / 2}, ${y}
+        C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+        Z`;
+      };
+      
+      const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+      
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+      };
+      
     if(isLoading) return <AnimatedLoader/>
     
     return (
@@ -55,12 +114,32 @@ const UserDashboard = () => {
         </div>
 
         <div className="min-h-[calc(100vh-300px)] w-full flex items-center justify-center text-center flex-col">
-            <h3 className="text-2xl md:text-5xl font-bold font-kanit">Welcome Back, <span className="text-main">{user?.displayName}</span></h3>
-            <h3 className="text-2xl md:text-4xl capitalize my-6 font-semibold font-kanit">Explorer your amazing dashboard</h3>    
+            <h2 className="font-poppins text-gray-700 dark:text-gray-400">Your chart with purchasing Class price</h2>
+        <ResponsiveContainer width="100%" height={500}>
+            <BarChart
+                data={userPayment}
+                margin={{
+                    top: 20,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="selectedClass" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="packagePrice" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                    {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                    ))}
+                </Bar>
+            </BarChart>  
+        </ResponsiveContainer>
         </div>
-        {/* <div className="my-16">
-            <ClassComparisonChart paymentData={allPaymentData}/>
-        </div> */}
+        
     </section>
     );
 };
